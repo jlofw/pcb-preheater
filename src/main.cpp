bd932a2fc8e6dfdc4c6475bf1916187b8d6d82c4
Joachim Löfwander <joachim.lofwander@gmail.com>
2021-05-27 21:54:41 +0200

    working rotary encoder test

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,45 @@
-#include <Arduino.h>
+#include <SimpleButton.h>
 
-void setup()
-{
-    // put your setup code here, to run once:
+using namespace simplebutton;
+
+/*
+   1 Rotary encoder is connected to a Wemos D1 mini (ESP8266)
+   - Rotary encoder "Key" (that's the button) is connected to gpio 12 (D6)
+   - Rotary encoder "S1" is connected to gpio 5 (D1) - that's channel A
+   - Rotary encoder "S4" is connected to gpio 4 (D2) - that's channel B
+   (the labels on your rotary encoder might be different!)
+ */
+
+RotaryEncoder* myEncoder = NULL;
+int32_t previousPosition = 0;
+
+void setup() {
+    Serial.begin(115200);
+    Serial.println();
+
+    myEncoder = new RotaryEncoder(1, 0, 2); // channel-A, channel-B, push button (255 = not used)
+    // myEncoder->setEncoding(2); // <- if it used x2 encoding (x1 is default)
+    myEncoder->setMin(0);
+    myEncoder->setMax(400);
+    // myEncoder->setInverted(true);
+    // myEncoder->enableLoop(true);
+
+    Serial.println("Started");
 }
 
-void loop()
-{
-    // put your main code here, to run repeatedly:
+void loop() {
+    myEncoder->update();
+
+    int32_t currentPosition = myEncoder->getPos();
+
+    if (currentPosition != previousPosition) {
+        previousPosition = currentPosition;
+        Serial.print(currentPosition);
+        if (myEncoder->incremented()) Serial.println(" up");
+        if (myEncoder->decremented()) Serial.println(" down");
+    }
+
+    if (myEncoder->clicked()) {
+        Serial.println("clicked");
+    }
 }
