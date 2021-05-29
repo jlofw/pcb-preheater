010542b051e9e027c138417f613eec2e3388e510
Joachim Löfwander <joachim.lofwander@gmail.com>
2021-05-29 13:06:31 +0200

    implemented isr for rotary click

--- src/main.cpp
+++ src/main.cpp
@@ -10,10 +10,12 @@ ENC_t encoder;
 int set_temp = 0;
 volatile byte flag_inc = 0;
 volatile byte flag_dec = 0;
+volatile byte flag_clicked = 0;
 
 void init_encoder_isr(PENC_t encoder_adr);
 void isr_clk();
 void isr_dt();
+void isr_sw();
 
 void setup()
 {
@@ -29,30 +31,40 @@ void loop()
 	if (flag_inc)
 	{
 		noInterrupts();
-		Serial.println("FIRED");
-		Serial.println(encoder_adr->count++);
+		encoder_adr->count++;
+		Serial.println(encoder_adr->count);
 		flag_inc = 0;
 		interrupts();
 	}
 	else if (flag_dec)
 	{
 		noInterrupts();
-		Serial.println("FIRED");
-		Serial.println(encoder_adr->count--);
+		encoder_adr->count--;
+		Serial.println(encoder_adr->count);
 		flag_dec = 0;
 		interrupts();
 	}
+	else if (flag_clicked)
+	{
+		noInterrupts();
+		encoder_adr->count = 0;
+		Serial.println(encoder_adr->count);
+		flag_clicked = 0;
+		interrupts();
+	}
+	delay(10);
 }
 
 void init_encoder_isr(PENC_t encoder_adr)
 {
 	attachInterrupt(digitalPinToInterrupt(encoder_adr->clk), isr_clk, FALLING);
 	attachInterrupt(digitalPinToInterrupt(encoder_adr->dt), isr_dt, FALLING);
+	attachInterrupt(digitalPinToInterrupt(encoder_adr->sw), isr_sw, LOW);
 }
 
 void isr_clk()
 {
-	if (digitalRead(1))
+	if (digitalRead(DT_pin))
 	{
 		flag_inc = 1;
 	}
@@ -60,8 +72,13 @@ void isr_clk()
 
 void isr_dt()
 {
-	if (digitalRead(0))
+	if (digitalRead(CLK_pin))
 	{
 		flag_dec = 1;
 	}
+}
+
+void isr_sw()
+{
+	flag_clicked = 1;
 }
